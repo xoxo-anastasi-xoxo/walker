f148ed9e4e7a0f26818a830a7667ca95e7b11661
Anastasi <nastya.k.r@mail.ru>
2018-02-12 09:37:06 +0300

    menu test

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react'
 import Header from '../components/Site/Header/Header'
 import Footer from "../components/Site/Footer/Footer";
+import "./Pages.css"
 
 class About extends Component {
     render() {
@@ -8,6 +9,9 @@ class About extends Component {
             <div className="page">
                 <Header/>
                 <p>Ну в это уже другое)</p>
+                <div className="tes">
+                <a href="javascript:void(0)" className="test"></a>
+                <div className="test-two"></div></div>
                 <Footer/>
             </div>
         );
